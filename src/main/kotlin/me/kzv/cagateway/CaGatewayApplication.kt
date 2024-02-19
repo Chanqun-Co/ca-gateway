6d8622585b4f3bdac7963f942f7bc3e4e40bc168
kimzerovirus <yg9699k@naver.com>
2024-02-19 21:47:59 +0900

    ca-gateway-#1 gateway서버 세팅

---
+++ src/main/kotlin/me/kzv/cagateway/CaGatewayApplication.kt
@@ -0,0 +1,11 @@
+package me.kzv.cagateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CaGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<CaGatewayApplication>(*args)
+}
---
+++ src/test/kotlin/me/kzv/cagateway/CaGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.kzv.cagateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CaGatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
